71cd395afa8691b7ca18f92f1b2417510c76b0cd
Jens Kristian <jenskristian@gmail.com>
2019-07-16 09:44:48 +0200

    don't send back tokens and password to client

--- src/index.js
+++ src/index.js
@@ -25,15 +25,3 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-  const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcource', { expiresIn: '7 days' })
-  console.log(token)
-
-  const data = jwt.verify(token, 'thisismynewcource')
-  console.log(data)
-}
-
-myFunction()
